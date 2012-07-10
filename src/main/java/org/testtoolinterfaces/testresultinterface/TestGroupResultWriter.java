4c66b10294922d4ef5c33e789b33371d55086db1
arjankranenburg <Arjan.Kranenburg@gmail.com>
2012-07-10 07:54:58 +0200

    Improved comment of write()

--- src/main/java/org/testtoolinterfaces/testresultinterface/TestGroupResultWriter.java
+++ src/main/java/org/testtoolinterfaces/testresultinterface/TestGroupResultWriter.java
@@ -8,7 +8,9 @@ import org.testtoolinterfaces.testresult.TestGroupResultObserver;
 public interface TestGroupResultWriter extends TestGroupResultObserver
 {
 	/**
-	 * To write the end result
+	 * To write the result.
+	 * Note the result may not be final: make sure to register this Observer at the
+	 *  TestGroupResult to be notified of updates.
 	 */
 	public abstract void write( TestGroupResult aTestGroupResult, File aResultFile );
 }
