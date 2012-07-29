c6e8c2491ac3fe2776314521fbdfb58f70e921f5
arjankranenburg <Arjan.Kranenburg@gmail.com>
2012-07-29 22:22:50 +0200

    Added static methods for reading TestCaseResult Files

--- src/main/java/org/testtoolinterfaces/testresultinterface/TestCaseResultReader.java
+++ src/main/java/org/testtoolinterfaces/testresultinterface/TestCaseResultReader.java
@@ -8,6 +8,7 @@ import javax.xml.parsers.SAXParserFactory;
 
 import org.testtoolinterfaces.testresult.TestCaseResult;
 import org.testtoolinterfaces.testresult.TestCaseResultLink;
+import org.testtoolinterfaces.testsuite.LooseTestInterfaceList;
 import org.testtoolinterfaces.testsuite.TestInterfaceList;
 import org.testtoolinterfaces.utils.Trace;
 import org.xml.sax.XMLReader;
@@ -68,5 +69,51 @@ public class TestCaseResultReader
 		}
 
 		return testCaseResult;
+	}
+	
+	/**
+	 * Reads the TestCaseResults.
+	 * 
+	 * @param aTestInterfaceList
+	 * @param tcResultLink
+	 * @return a TestCaseResult
+	 */
+	public static TestCaseResult read( TestInterfaceList aTestInterfaceList, TestCaseResultLink tcResultLink )
+	{
+		TestCaseResultReader tcResultReader = new TestCaseResultReader( aTestInterfaceList );
+		return tcResultReader.readTcResultFile(tcResultLink);
+	}
+
+	/**
+	 * Reads the TestCaseResults.
+	 * 
+	 * @param aTestInterfaceList
+	 * @param tcResultFile
+	 * @return a TestCaseResult
+	 */
+	public static TestCaseResult read( TestInterfaceList aTestInterfaceList, File tcResultFile )
+	{
+		TestCaseResultReader tcResultReader = new TestCaseResultReader( aTestInterfaceList );
+		return tcResultReader.readTcResultFile(tcResultFile);
+	}
+
+	/**
+	 * Reads the TestCaseResults without verifying if the commands belonged to an interface
+	 * @param tcResultLink
+	 * @return a TestCaseResult
+	 */
+	public static TestCaseResult read( TestCaseResultLink tcResultLink )
+	{
+		return TestCaseResultReader.read(new LooseTestInterfaceList(), tcResultLink);
+	}
+
+	/**
+	 * Reads the TestCaseResults without verifying if the commands belonged to an interface
+	 * @param tcResultFile
+	 * @return a TestCaseResult
+	 */
+	public static TestCaseResult read( File tcResultFile )
+	{
+		return TestCaseResultReader.read(new LooseTestInterfaceList(), tcResultFile);
 	}
 }
